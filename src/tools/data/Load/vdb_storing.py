d2e1dbb69e303d05b604d7c8a14426c37de27624
MathiasGagnon <Mathias.Gagnon@USherbrooke.ca>
2024-05-26 09:15:46 -0400

    untested pinecone integration

--- src/tools/data/Load/vdb_storing.py
+++ src/tools/data/Load/vdb_storing.py
@@ -3,21 +3,39 @@ from dotenv import load_dotenv
 import os
 
 #TODO: Save real information
+#=======================================================
+#TODO: UNTESTEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+#=======================================================
 
 class vdb_storing():
     def __init__():
         # Load environment variables from .env file
         load_dotenv()
         # Access the API key
-        api_key = os.getenv('PINECONE_API_KEY')
+        self.api_key = os.getenv('PINECONE_API_KEY')
+        self.pc = Pinecone(api_key=self.api_key)
+        self.index_name = "parallaile-index"
 
-        pc = Pinecone(api_key=api_key)
-        pc.create_index(
-            name="quickstart",
-            dimension=8, # Replace with your model dimensions
-            metric="euclidean", # Replace with your model metric
-            spec=ServerlessSpec(
-                cloud="aws",
-                region="us-east-1"
-            ) 
-        )
+    def first_setup():
+        pc = self.pc
+        index_name = self.index_name
+        if index_name not in pc.list_indexes().names():
+            pc.create_index(
+                name=index_name,
+                dimension=1536,
+                metric="cosine",
+                spec=ServerlessSpec(
+                    cloud='aws', 
+                    region='us-east-1'
+                ) 
+            )
+
+    def write_vector(id, values, namespace):
+        index = pc.Index(self.index_name)
+
+        index.upsert(
+            vectors=[
+                {"id": id, "values": values}
+            ],
+            namespace=namespace
+            )
